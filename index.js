37c1589b8580e06ba4b62830d010e9ad5f41804f
Zachary Truong <zacharytruong@gmail.com>
2022-11-04 01:42:50 -0500

    update all settings

---
+++ index.js
@@ -0,0 +1,22 @@
+const messages = [
+	{
+		name: 'John',
+		age: 27,
+		books: [
+			{
+				author: 'Brian',
+				genres: {
+					names: ['fiction'],
+				},
+			},
+		],
+	},
+	{
+		name: 'Rock',
+		age: 35,
+	},
+];
+
+const sayHi = (name) => console.log(`${name}`);
+
+sayHi();
